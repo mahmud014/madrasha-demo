9a218309cc061a43e2df4b56908e80ac886f4e9d
mahmud014 <anik955720@gmail.com>
2026-03-30 11:02:22 +0600

    add fix some folder error

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,14 @@
-import AdminSidebar from '@/components/admin/AdminSidebar';
+import AdminSidebar from "@/components/admin/AdminSidebar";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex min-h-screen">
       <AdminSidebar />
-      <div className="flex-1 p-8">
-        {children}
-      </div>
+      <div className="flex-1 p-8">{children}</div>
     </div>
   );
 }
